404ad5101a0eb428fdc3d806319a51d064ce0851
Bronya-Rand <azarieldc@gmail.com>
2024-04-01 06:08:25 +0100

    fix: readjust if statement

--- renpy/mod_docker.py
+++ renpy/mod_docker.py
@@ -71,8 +71,8 @@ class ModDockerMain(object):
                     
                 archives.append("mods/{}/game/{}".format(self.mod_name, base))
             
-            if os.path.exists(os.path.join(renpy.config.basedir, "game", "mod_patches.rpa")):
-                archives.append("mod_patches")
+        if os.path.exists(os.path.join(renpy.config.basedir, "game", "mod_patches.rpa")):
+            archives.append("mod_patches")
 
         archives.reverse()
 
